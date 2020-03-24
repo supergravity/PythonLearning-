c9697ca2a0b048b8ac7200d9646ada8cae656811
Tzu-Lu Lin <liampassion6626@gmail.com>
2020-03-24 16:36:27 +0800

    add files

---
+++ Python/example1_01.py
@@ -0,0 +1,8 @@
+print('The Spanish of apple?', end='    ')
+ans = input()
+print('The answer you entered is :' + ans)
+
+if ans == 'manzana':
+    print('Good Job ~')
+else:
+    print('Try Again ~')
---
+++ Python/example2_02.py
@@ -0,0 +1,11 @@
+#   
+
+print('Which company did Elon Musk found?')
+ans = input()
+
+#   Argument
+
+if ans == 'Tesla' or ans == 'SpaceX':
+    print('Good Job')
+else:
+    print('Try Again ~')
