f853260ab45c530b11bd6a70b9cc5e485439f794
Tzu-Lu Lin <liampassion6626@gmail.com>
2020-04-04 21:42:26 +0800

    add files

--- Python/example3_13_function.py
+++ Python/example3_13_function.py
@@ -3,6 +3,27 @@ from random import choice
 
 def words (status=True):
     right = ['Nice~','Great','Awesome','Bingo']
-    wrong = ['Try Again','']
+    wrong = ['Try Again','Dont Give Up','Oops']
+
+    if status:
+        msg = choice(right)
+    else:
+        msg = choice(wrong)
+
+    return msg
+
+a_list = ['SpaceX','Tesla','Neurallink']
+
+
+print('What company did Elon Musk found?')
+
+ans = input().strip()
+
+if ans in a_list:
+    print(words(True))
+else:
+    print (words(False))
+
+
 
 
